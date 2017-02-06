f2a03befccc0ad84003eeda4f5cf2c67dd28f917
simmarith <menzeladrian@googlemail.com>
2017-02-07 00:44:19 +0100

    property reader GlobalProperties added
    will come into use pretty soon

---
+++ src/com/simmarith/utils/GlobalProperties.java
@@ -0,0 +1,32 @@
+package com.simmarith.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class GlobalProperties {
+	// Properties
+	private static GlobalProperties instance;
+	private static Properties props;
+	
+	//Methods
+	public void initProperties(String fileName) {
+		if (GlobalProperties.instance == null) {
+			GlobalProperties.instance = new GlobalProperties(fileName);
+		}
+	}
+	
+	public static String getProperty(String prop) {
+		return GlobalProperties.props.getProperty(prop);
+	}
+
+	// Constructors
+	private GlobalProperties(String fileName) {
+		try {
+			props.load(new FileInputStream(fileName));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
