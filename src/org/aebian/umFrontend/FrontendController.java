d29b26d6c779d23eca3ac63ea9e32620391c6d52
Alexander Knight <alexander.knight@knight-industries.org>
2017-02-08 09:32:06 +0100

    Redesigned icon structure
    still work in progress...

--- src/org/aebian/umFrontend/FrontendController.java
+++ src/org/aebian/umFrontend/FrontendController.java
@@ -5,6 +5,10 @@ import javafx.scene.control.Label;
 
 public class FrontendController {
 
+    static String AppImage = Frontend.class.getClassLoader().getResource("res/images/uMgmt.png").toString();
+
+
+
     public Label umName;
     public Label umPass;
 }
--- src/org/aebian/umFrontend/FrontendView.java
+++ src/org/aebian/umFrontend/FrontendView.java
@@ -15,7 +15,7 @@ public class FrontendView extends Application {
 
         Scene scene = new Scene(root, 600, 500);
 
-        primaryStage.getIcons().add(new Image("/org/aebian/umFrontend/icons/uMgmt.png"));
+        primaryStage.getIcons().add(new Image( FrontendController.AppImage));
         primaryStage.setTitle("User Management");
         primaryStage.setScene(scene);
         primaryStage.show();
