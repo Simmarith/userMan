bea62d27b615d5ea84c0f5a33bb550969b7c0c6a
Alexander Knight <alexander.knight@knight-industries.org>
2017-02-14 14:00:14 +0100

    current state of UI
    Current state of the UI

--- src/org/aebian/umFrontend/view/FrontendView.java
+++ src/org/aebian/umFrontend/view/FrontendView.java
@@ -2,18 +2,18 @@
 package org.aebian.umFrontend.view;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
-import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.stage.*;
 
 import java.io.IOException;
 
@@ -22,15 +22,15 @@ public class FrontendView extends Application {
 
     protected static Button btnLogin, btnSave, btnDiscard, btnRefresh, btnEdit;
     private Stage primaryStage;
-    private VBox vLogin, vUmgmt, vUedit;
     private BorderPane rootLayout;
 
     @Override
     public void start(Stage primaryStage) {
 
         GridPane root = new GridPane();
-        root.setStyle("-fx-background-color: #4580BA;");
         primaryStage.setMinWidth(800);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+
 
         primaryStage.setMinHeight(500);
         primaryStage.setResizable(false);
@@ -38,13 +38,12 @@ public class FrontendView extends Application {
 
         primaryStage.setTitle("User Management");
         Scene scene = new Scene(root, 800, 500);
-        primaryStage.setScene(scene);
 
-        //primaryStage.show();
+        primaryStage.setScene(scene);
         this.primaryStage = primaryStage;
         root.setAlignment( Pos.CENTER );
-        initRootLayout();
 
+        initRootLayout();
         showLogin();
 
     }
@@ -55,14 +54,34 @@ public class FrontendView extends Application {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(FrontendView.class.getResource("UI/umFrontendRoot.fxml"));
             rootLayout = (BorderPane) loader.load();
+            rootLayout.setId("umFrontend");
 
             // Show the scene containing the root layout.
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
+            String css = FrontendView.class.getResource("UI/umFrontendStyles.css").toExternalForm();
+            scene.getStylesheets().clear();
+            scene.getStylesheets().add(css);
             primaryStage.show();
+
+            MenuItem exit = new MenuItem("Exit");
+            exit.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent t) {
+                    System.exit(0);
+                }
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+
+    /**
+     * Closes the application.
+     */
+    @FXML
+    private void handleExit() {
+        System.exit(0);
     }
 
 
@@ -72,7 +91,6 @@ public class FrontendView extends Application {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(FrontendView.class.getResource("UI/umFrontendLogin.fxml"));
             VBox showLogin = (VBox) loader.load();
-
             // Set login to center of root layout.
             rootLayout.setCenter(showLogin);
         } catch (IOException e) {
